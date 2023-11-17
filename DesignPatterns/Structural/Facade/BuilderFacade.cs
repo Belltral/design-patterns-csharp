b508db8d13c8167eb67d1df555cdb36b5bc21ee0
Henrique Reis <henriquereis198@outlook.com>
2023-11-17 10:46:00 -0300

    Facade

---
+++ DesignPatterns/Structural/Facade/BuilderFacade.cs
@@ -0,0 +1,25 @@
+﻿
+using DesignPatterns.Creational.Builder.Classes;
+
+namespace DesignPatterns.Structural.Facade
+{
+    public class BuilderFacade
+    {
+        // Fachada para a pasta Creational/Builder
+        private MainDishBuilder mainDishBuilder = new MainDishBuilder();
+
+        public void MakeMeal1()
+        {
+            mainDishBuilder.MakeMeal();
+            Console.Write(mainDishBuilder.GetMeal());
+            Console.WriteLine(mainDishBuilder.GetPrice());
+            Console.WriteLine();
+        }
+
+        public void MakeMeal2()
+        {
+            mainDishBuilder.Reset();
+            Console.WriteLine(mainDishBuilder.MakeBeverage());
+        }
+    }
+}
